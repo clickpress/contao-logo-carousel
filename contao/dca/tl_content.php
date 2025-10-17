20ad3f1905d8c9fb00eb56fe46e7efeac85313ae
stefansl <ssl@clickpress.de>
2025-10-17 12:42:05 +0200

    Add 'fullsize' option to 'logo_carousel' image legend in tl_content configuration

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -2,6 +2,6 @@
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['logo_carousel'] = '
     {type_legend},type,headline;
-    {image_legend},multiSRC, size;
+    {image_legend},multiSRC, size, fullsize;
     {expert_legend:hide},cssID;
 ';
